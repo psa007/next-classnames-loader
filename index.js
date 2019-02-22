9e0837721f65bba7b570da6e8275a5fbcfe94810
Jérémy Barbet <jeremgraph@gmail.com>
2019-02-22 11:31:48 +0000

    Change to cwd path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports.pitch = function(remainingRequest) {
     this.cacheable();
     return `
         // classnames-loader: automatically bind css-modules to classnames
-        var classNames = require(${loaderUtils.stringifyRequest(this, '!' + require.resolve('classnames/bind'))});
+        var classNames = require('${process.cwd()}/node_modules/classnames/bind');
         var locals = require(${loaderUtils.stringifyRequest(this, '!!' + remainingRequest)});
         var css = classNames.bind(locals);
         for (var style in locals) css[style] = locals[style];
